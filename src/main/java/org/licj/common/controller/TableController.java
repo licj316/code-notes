a96496b9c8b0a65c68e22a28264f82383118e881
licj316 <iskz271@163.com>
2017-03-01 14:10:39 +0800

    fixes bug

--- src/main/java/org/licj/common/controller/TableController.java
+++ src/main/java/org/licj/common/controller/TableController.java
@@ -1,16 +1,15 @@
 package org.licj.common.controller;
 
 
-import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import javax.servlet.http.HttpSessionEvent;
 
+import org.licj.common.service.TableService;
+import org.licj.common.vo.JSONParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -18,14 +17,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.cds.contract.common.vo.JSONParam;
-import com.cds.common.service.TableService;
-import com.cds.ucenter.web.utils.UserUtils;
-
 
 @Controller
 @RequestMapping("/table")
-public class TableController extends BaseController {
+public class TableController {
 
 	private static final Logger log = LoggerFactory
 			.getLogger(TableController.class);
@@ -45,8 +40,8 @@ public class TableController extends BaseController {
 				}
 			}
 			//控制权限--插入当前用户
-			Long currUserId = UserUtils.getCurrUserIdByCookie(request);
-			vo.put("currUserId", currUserId);
+			//Long currUserId = UserUtils.getCurrUserIdByCookie(request);
+			//vo.put("currUserId", currUserId);
 			resultMap = tableService.getTableData(vo);
 			return resultMap;
 		} catch (Exception e) {
@@ -61,25 +56,7 @@ public class TableController extends BaseController {
 	@RequestMapping(value = "/exportExcel", method = RequestMethod.POST)
 	public void exportExcel(HttpServletRequest request, HttpServletResponse response) {
 		String exportConfig = request.getParameter("exportConfig");//new String(request.getParameter("exportConfig").getBytes("iso-8859-1"), "utf-8");
-		Long currUserId = UserUtils.getCurrUserIdByCookie(request);
-		tableService.exportExcel(response, exportConfig, currUserId);
+		//Long currUserId = UserUtils.getCurrUserIdByCookie(request);
+		tableService.exportExcel(response, exportConfig, 0L);
 	}
-	
-	/*@RequestMapping(value = "/getPercent", method = RequestMethod.POST)
-	public Map<String, Object> getPercent(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		HttpSession session = request.getSession();
-		int uploadPercent = Integer.parseInt(session.getAttribute("uploadPercent").toString());
-		Map<String, Object> resMap = new HashMap<String, Object>();
-		resMap.put("uploadPercent", uploadPercent);
-		return resMap;
-	}
-	
-	@RequestMapping(value = "/setPercent", method = RequestMethod.POST)
-	public void setPercent(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		HttpSession session = request.getSession();
-		for (int i = 0; i < 100; i++) {
-			Thread.sleep(1000);
-			session.setAttribute("uploadPercent", i);
-		}
-	}*/
 }
